fc71bbd0bab84f2c2939e23fe3b034981f9fd352
Ramy Kfoury <kfoury.r@gmail.com>
2015-10-24 21:43:34 +0200

    Initial Commit

---
+++ StashConnector/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StashConnector
+//
+//  Created by Ramy Kfoury on 24/10/15.
+//  Copyright © 2015 Ramy Kfoury. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
